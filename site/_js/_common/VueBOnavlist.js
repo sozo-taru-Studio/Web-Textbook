cbb5bebd1e1032442ecd8561fd9823c5a48cb415
magotora <magotora@gmail.com>
2019-06-11 18:27:36 +0900

    vue.js のカスタマイズ

---
+++ site/_js/_common/VueBOnavlist.js
@@ -0,0 +1,53 @@
+// basic Operation - Web-Textbook リスト生成
+var BOlists = new Vue({
+    el: '#w-BOlist',
+    data: {
+         titles:[
+                { titlename:'電源とアプリケーション',
+                    titleen:"power on off",
+                    url:"00_basic_operation.html"
+                },
+                {
+                    titlename:"マウス操作",
+                    titleen:"mouse",
+                    url:"01_basic_operation.html" 
+                },
+                {
+                    titlename:"キーボード操作",
+                    titleen:"keybord",
+                    url:"02_basic_operation.html"
+                },
+                {
+                    titlename:"USBメモリーデバイスの使い方",
+                    titleen:"mobileDrive",
+                    url:"03_basic_operation.html" 
+                },
+                {
+                    titlename:"基本的なキーボードショートカット",
+                    titleen:"keybord-Shortcut",
+                    url:"04_basic_operation.html" }
+                ]
+   }
+  });
+ // web概論
+  var OLlists = new Vue({
+    el: '#w-OLlist',
+    data: {
+         titles:[
+                { titlename:'メディア研究',
+                    titleen:"Whats Media!",
+                    url:"10_outline_media.html"
+                },
+                {
+                    titlename:'メディアの特徴を理解し分類してみる',
+                    titleen:"Media type",
+                    url:"11_outline_media.html"
+                },
+                {
+                    titlename:'webの役割',
+                    titleen:"web rol",
+                    url:"12_outline_media.html"
+                }
+            ]
+   }
+  });
